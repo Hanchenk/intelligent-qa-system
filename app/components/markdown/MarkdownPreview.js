c040e77c76675bc2a12fc92c7a9a8fb18e24017b
Jihy <Jihy@yemast.com>
2025-04-21 11:54:04 +0800

    恢复收藏练习

---
+++ app/components/markdown/MarkdownPreview.js
@@ -0,0 +1,8 @@
+'use client';
+
+import MarkdownPreviewComponent from '../MarkdownPreview';
+import OptionMarkdownPreviewComponent from '../OptionMarkdownPreview';
+
+// 重新导出组件
+export const MarkdownPreview = MarkdownPreviewComponent;
+export const OptionMarkdownPreview = OptionMarkdownPreviewComponent; 
